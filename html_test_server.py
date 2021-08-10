7f3b272a250666363931529707325122e1c4a1c7
ryley-matos <mail@ryley.io>
2021-08-10 13:03:57 -0700

    html_test_server now creates img using imgkit when servers index is requested

--- html_test_server.py
+++ html_test_server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from reddit import reddit, get_comment_html
+import imgkit
 
 app = Flask(__name__)
 
@@ -8,7 +9,9 @@ TEST_ID = 'h8arrkx'
 @app.route('/')
 def test():
     comment = reddit.comment(TEST_ID)
-    return get_comment_html(comment, '')
+    html = get_comment_html(comment, '')
+    imgkit.from_string(html, 'out.jpg', options={'width': 600})
+    return html 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
