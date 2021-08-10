57241ae6bf08c7d5ce59d5ac66d2183543ffb1b3
ryley-matos <mail@ryley.io>
2021-08-10 15:48:58 -0700

    Added simple logging to bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == '__main__':
+    print('listening..')
     all_reddit = reddit.subreddit("all")
     for comment in all_reddit.stream.comments():
         if ('!Screenshot' in comment.body):
+            print('comment found!')
             create_comment_image(comment)
             
 
